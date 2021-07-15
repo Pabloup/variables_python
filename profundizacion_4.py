9fbc360aa657117415ec95939ddaff7943ab7623
Pabloup <87155896+Pabloup@users.noreply.github.com>
2021-07-15 00:11:04 -0300

    ejercicios de profundización

---
+++ profundizacion_4.py
@@ -0,0 +1,71 @@
+# Tipos de variables [Python]
+# Ejercicios de profundización
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# NOTA: 
+# Estos ejercicios son de mayor dificultad que los de clase y práctica.
+# Están pensados para aquellos con conocimientos previo o que dispongan
+# de mucho más tiempo para abordar estos temas por su cuenta.
+# Requiere mayor tiempo de dedicación e investigación autodidacta.
+
+# IMPORTANTE: NO borrar los comentarios en VERDE o NARANJA
+
+# Ejercicios de práctica numérica y cadenas
+'''
+Enunciado:
+Realice un programa que determine cual sería el apellido de una persona
+al ingresara los dos nombres completos de sus padres.
+En definitiva se solicita crear una variable nueva que reuna
+los apellidos.
+
+- Primero el programa debe consultar el nombre completo del padre_1
+- Luego el programa debe consultar el nombre completo del padre_2
+- Luego debe consultar el nombre del hijo/a
+- Debe extraer los apellidos de los padres (ver la nota al final)
+- Luego formar el nombre completo del hijo/a utilizando los apellidos
+  de sus padres y el nombre ingresado de dicha persona
+- Imprimir en pantalla el resultado
+
+NOTA: Para extraer el apellido del nombre completo recomendamos usar
+el método "split"
+Mostraremos un ejemplo:
+
+direccion_completa = 'Monroe 2716'
+calle, altura = direccion_completa.split(' ')
+
+Les dejo por su cuenta a que busquen un poco más acerca de este método
+que seguramente utilizarán mucho de acá en adelante.
+Les dejamos un link con algunos ejemplos más:
+https://www.pythonforbeginners.com/dictionary/python-split
+
+Cualquier duda con el método split pueden consultarla por el campus
+'''
+
+print('Jugando con texto')
+# Empezar aquí la resolución del ejercicio
+
+print("nombre completo del padre_1")
+padre_1 = str(input())
+print("nombre completo del padre 1" , padre_1)
+
+print("nombre completo del padre_2")
+padre_2 = str(input())
+print("nombre completo del padre 2" , padre_2)
+
+print("nombre del hijo")
+hijo = str(input())
+print("nombre del hijo" , hijo)
+
+padre_1 = "marcelo gonzalez"
+nombre, apellido1 = padre_1.split()
+print("apellido del padre_1" , apellido1)
+
+padre_2 = "romina gomez"
+nombre, apellido2 = padre_2.split()
+print("apellido del padre 2" , apellido2)
+
+print("nombre del hijo con apellido de los dos padres")
+nombre_completo = hijo + " " + apellido1 + " " + apellido2
+print("nombre del hijo con el apellido de los dos padres" , nombre_completo)
